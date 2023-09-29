3e7196c50d940e32505a3ff5a37d706b4ff2fc4e
juliocjuri <juliocesarjuriolli@gmail.com>
2023-09-29 08:51:45 -0300

    feat: google maps config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
+import { GOOGLE_MAPS_API_KEY } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'infrareport';
+
+  ngOnInit(): void {
+    let loader = new Loader({
+      apiKey: GOOGLE_MAPS_API_KEY,
+    });
+
+    loader.load().then(() => {
+      new google.maps.Map(document.getElementById('map') as HTMLElement, {
+        center: { lat: 51, lng: 6 },
+        zoom: 15,
+      });
+    });
+  }
 }
