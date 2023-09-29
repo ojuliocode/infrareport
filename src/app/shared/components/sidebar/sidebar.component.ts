fe136c3eff49827a1c69161fcb0365c37321fe24
juliocjuri <juliocesarjuriolli@gmail.com>
2023-09-29 11:28:02 -0300

    feat: started sidebar

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { SidebarItem } from '../../models/sidebar-item.model';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {
-
+export class SidebarComponent implements OnInit {
+  sidebarItems: SidebarItem[] = [];
+  ngOnInit(): void {
+    this.sidebarItems = [
+      {
+        label: 'Home',
+        icon: 'home',
+        height: 20,
+      },
+      {
+        label: 'Ocurrences',
+        icon: 'data_usage',
+        height: 20,
+      },
+    ];
+  }
 }
