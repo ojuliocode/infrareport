20bc0b94dd2035d9a4ad3921787c1042134c8df1
juliocjuri <juliocesarjuriolli@gmail.com>
2023-10-05 08:05:27 -0300

    fix: sidebar on mobile

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -9,6 +9,8 @@ import { SidebarItem } from '../../models/sidebar-item.model';
 })
 export class SidebarComponent implements OnInit {
   sidebarItems: SidebarItem[] = [];
+  sidebarCitizenItems: SidebarItem[] = [];
+  sidebarTownItems: SidebarItem[] = [];
   sideBarCustomItem: SidebarItem = {
     label: 'Sair',
     icon: 'logout',
@@ -16,15 +18,49 @@ export class SidebarComponent implements OnInit {
   };
 
   ngOnInit(): void {
-    this.sidebarItems = [
+    this.sidebarCitizenItems = [
       {
-        label: 'Ocorrências',
+        label: 'Home',
+        icon: 'home',
+        height: '5em',
+      },
+      {
+        label: 'Criar',
+        icon: 'plus',
+        height: '5em',
+      },
+      {
+        label: 'Filtrar',
+        icon: 'sort',
+        height: '5em',
+      },
+    ];
+    this.sidebarTownItems = [
+      {
+        label: 'Home',
         icon: 'home',
         height: '5em',
       },
       {
         label: 'Gráficos',
         icon: 'data_usage',
+        height: '5em',
+      },
+      {
+        label: 'Filtrar',
+        icon: 'sort',
+        height: '5em',
+      },
+    ];
+    this.sidebarItems = [
+      {
+        label: 'Gráficos',
+        icon: 'data_usage',
+        height: '5em',
+      },
+      {
+        label: 'Home',
+        icon: 'home',
         height: '5em',
       },
       {
