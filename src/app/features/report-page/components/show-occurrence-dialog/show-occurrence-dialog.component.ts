67af12d597cd7f474a71b187fd26024959928529
ojuliocode <juliojuriollif@gmail.com>
2024-01-24 20:51:36 -0300

    feat (wip): show occurence dialog

--- src/app/features/report-page/components/show-occurrence-dialog/show-occurrence-dialog.component.ts
+++ src/app/features/report-page/components/show-occurrence-dialog/show-occurrence-dialog.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { UntypedFormGroup } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Data } from '@angular/router';
+import { OCCURRENCE_TYPES } from 'src/app/shared/constants/occurrence.constants';
+import { Occurrence } from 'src/app/shared/models/occurrence.model';
 
 @Component({
   selector: 'app-show-occurrence-dialog',
@@ -8,7 +11,16 @@ import { Data } from '@angular/router';
   styleUrls: ['./show-occurrence-dialog.component.scss'],
 })
 export class ShowOccurrenceDialogComponent implements OnInit {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: Data) {}
-
+  occurrenceTypes = OCCURRENCE_TYPES;
+  createOcurrenceForm: UntypedFormGroup;
+  public occurrence: Occurrence = this.data['occurrence'];
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: Data,
+    public dialogRef: MatDialogRef<ShowOccurrenceDialogComponent>
+  ) {}
   ngOnInit(): void {}
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
 }
