791451d1c4f4865e82c2389065b65863471af64f
ojuliocode <juliojuriollif@gmail.com>
2024-01-31 19:23:55 -0300

    fix: sidebar buttons

--- src/app/shared/constants/occurrence.constants.ts
+++ src/app/shared/constants/occurrence.constants.ts
@@ -1,4 +1,7 @@
 export const OCCURRENCE_TYPES = [
   { value: 'lightsOut', label: 'Apagões' },
   { value: 'heavyRain', label: 'Chuvas fortes' },
+  { value: 'floodRisk', label: 'Risco de enchente' },
+  { value: 'cloggedManhole', label: 'Bueiro Entupido' },
+  { value: 'damagedPole', label: 'Poste Danificado' },
 ];
